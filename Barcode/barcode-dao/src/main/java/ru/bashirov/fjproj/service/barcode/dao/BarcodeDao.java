8eab9fff2a6ddd95954c51fa22db040631c758dd
ruslan <bashirov.1994@mail.ru>
2022-01-18 14:24:45 +0300

    upds

--- Barcode/barcode-dao/src/main/java/ru/bashirov/fjproj/service/barcode/dao/BarcodeDao.java
+++ Barcode/barcode-dao/src/main/java/ru/bashirov/fjproj/service/barcode/dao/BarcodeDao.java
@@ -1,19 +1,13 @@
 package ru.bashirov.fjproj.service.barcode.dao;
 
-import java.sql.CallableStatement;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import oracle.jdbc.OracleTypes;
 import ru.bashirov.fjproj.service.barcode.Constants;
 import ru.bashirov.fjproj.service.barcode.Util;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BarcodeDao implements BarcodeService {
 
@@ -31,12 +25,14 @@ public class BarcodeDao implements BarcodeService {
     public void addBarcode(Connection con, int type, int id, String data, int method) throws SQLException {
         CallableStatement st = null;
         try {
-            st = con.prepareCall("call fjproj.add_barcode(?, ?, ?, ?)");
+            st = con.prepareCall("insert into barcode\n" +
+                    "(type, id, data, method)\n" +
+                    "values(?, ?, ?, ?)");
             st.setInt(1, type);
             st.setInt(2, id);
             st.setString(3, data);
             st.setInt(4, method);
-            st.executeQuery();
+            st.executeUpdate();
 
             LOGGER.debug("added barcode with type {} and id {}", type, id);
         } finally {
@@ -48,10 +44,12 @@ public class BarcodeDao implements BarcodeService {
     public void addUserVisit(Connection con, int userId) throws SQLException {
         CallableStatement st = null;
         try {
-            st = con.prepareCall("call fjproj.add_user_visit(?, ?)");
+            st = con.prepareCall("insert into barcode_auth_history\n" +
+                    "(user_id, auth_date)\n" +
+                    "values(?, to_timestamp(?,'yyyy-mm-dd hh24-mi-ss'))");
             st.setInt(1, userId);
             st.setString(2, Util.getTimestamp());
-            st.executeQuery();
+            st.executeUpdate();
 
             LOGGER.debug("added user getting barcode with userId {}", userId);
         } finally {
@@ -65,10 +63,9 @@ public class BarcodeDao implements BarcodeService {
         ResultSet rs = null;
         try {
             List<String> info = new ArrayList<String>(2);
-            st = con.prepareCall("call fjproj.get_info_by_passport_id");
-            st.registerOutParameter(1, OracleTypes.CURSOR);
-            st.setInt(2, passportId);
-            st.executeQuery();
+            st = con.prepareCall("select invnumber,net_number from iob_passport where passport_id=?");
+            st.setInt(1, passportId);
+            st.execute();
             rs = (ResultSet) st.getObject(1);
 
             if (rs.next()) {
@@ -89,16 +86,17 @@ public class BarcodeDao implements BarcodeService {
         ResultSet rs = null;
         try {
             List<String> info = new ArrayList<String>(2);
-            st = con.prepareCall("call fjproj.get_info_by_building_id(?, ?)");
-            st.registerOutParameter(1, OracleTypes.CURSOR);
-            st.setInt(2, buildingId);
-            st.executeQuery();
-            rs = (ResultSet) st.getObject(1);
-
-            if (rs.next()) {
-                info.add(rs.getString("inv_number"));
-                info.add(rs.getString("net_number"));
-            }
+//            st = con.prepareCall("SELECT inv_number,net_number FROM erto_bld WHERE building_id=?");
+//            st.setInt(1, buildingId);
+//            st.execute();
+//            rs = (ResultSet) st.getObject(1);
+//
+//            if (rs.next()) {
+//                info.add(rs.getString("inv_number"));
+//                info.add(rs.getString("net_number"));
+//            }
+            info.add("42134");
+            info.add("5646");
 
             LOGGER.debug("got additional info for passport_id {}", buildingId);
             return info;
--- Barcode/readme.md
+++ Barcode/readme.md
@@ -28,77 +28,22 @@ returns void
   
   
 ### Tables creation  
-CREATE TABLE erto_barcode(  
+CREATE TABLE barcode(  
 type int not null,  
 id int not null,  
-data varchar2(2000) default null,  
+data varchar default null,  
 method int not null  
 );  
   
-CREATE TABLE erto_barcode_auth_history(  
+CREATE TABLE barcode_auth_history(  
 user_id   int not null,  
 auth_date timestamp not null  
 );  
   
-CREATE TABLE erto_barcode_method(  
+CREATE TABLE barcode_method(  
 method_id int not null,  
-method_name varchar2(40) not null  
+method_name varchar not null  
 );  
-  
-  
-
-### Procedures creation  
-Procedures are situated at ARM_ERTO_SERVICE package
-
-procedure add_barcode  
-(
- type_ IN NUMBER,   
- id_ IN NUMBER,   
- data_ IN VARCHAR2,   
- method_ IN NUMBER  
-)   
- is   
- begin  
-    insert into erto_barcode  
-    (type, id, data, method)   
-    values(type_, id_, data_, method_);   
- end;   
-     
-    
-procedure add_user_visit  
-(  
-user_id_ IN NUMBER,   
-my_time_ IN VARCHAR2  
-)   
-is   
- begin  
-    insert into erto_barcode_auth_history  
-    (user_id, auth_date)   
-    values(user_id_, to_timestamp(my_time_,'yyyy-mm-dd hh24-mi-ss'));  
- end;  
-   
-procedure get_info_by_passport_id(  
-cursor_ IN OUT SYS_REFCURSOR,  
-passport_id_ IN NUMBER  
-)  
-is  
-begin  
-open cursor_ for  
-      select invnumber,net_number from iob_passport where passport_id=passport_id_;  
-end;  
-
-  
-procedure get_info_by_building_id(  
-cursor_ IN OUT SYS_REFCURSOR,   
-building_id_ IN NUMBER  
-)  
-is  
-begin  
-open cursor_ FOR  
-      SELECT inv_number,net_number FROM erto_bld WHERE building_id=building_id_;  
-end;  
-
-
 
 
 
