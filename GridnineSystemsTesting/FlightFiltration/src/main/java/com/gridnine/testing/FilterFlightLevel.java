f50e36dd97a5f2bf4dd63a5ce9caeef468008841
ruslan <bashirov.1994@mail.ru>
2022-05-03 13:41:39 +0300

    rm some folders

--- GridnineSystemsTesting/FlightFiltration/src/main/java/com/gridnine/testing/FilterFlightLevel.java
+++
@@ -1,9 +0,0 @@
-package com.gridnine.testing;
-
-public abstract class FilterFlightLevel {
-	FilterFlightLevel() {
-		FiltrationBuilder.addFilterFlightLevel(this);
-	}
-	
-	public abstract boolean ifFilter(Flight flight);
-}
--- GridnineSystemsTesting/FlightFiltration/src/main/java/com/gridnine/testing/FilterSegmentLevel.java
+++
@@ -1,9 +0,0 @@
-package com.gridnine.testing;
-
-public abstract class FilterSegmentLevel {
-	FilterSegmentLevel() {
-		FiltrationBuilder.addFilterSegmentLevel(this);
-	}
-	
-	public abstract boolean ifFilter(Segment segment);
-}
